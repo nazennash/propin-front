22448b83f9f447157fa7a117f49bc2f8f5c53a5f
nazennash <nazennash120@gmail.com>
2024-08-14 16:25:56 +0300

    api-route-change

--- src/apiconfig.jsx
+++ src/apiconfig.jsx
@@ -1,8 +1,10 @@
-// apiconfig.jsx
 import axios from 'axios';
 
+// const API_URL = "http://127.0.0.1:8000/";
+const API_URL = "https://pinacore-rnlyj.ondigitalocean.app/";
+
 export const axiosPrivateInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
